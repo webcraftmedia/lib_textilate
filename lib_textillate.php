5248bfd0447dd4a26d20ff7cdab65d5b9622ce77
Messerbill <ole.hoppe@gmx.de>
2015-06-09 14:09:57 +0200

    added lib_textillate

---
+++ lib_textillate.php
@@ -0,0 +1,10 @@
+<?php
+namespace LIB;
+class lib_textillate extends \LIB\lib_js{
+    public static function get_class(){
+        return self::class;}
+    public static function js_path(){
+        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'tablesorter/lib/jquery.textillate.js');}
+    public static function version(){
+        return '<a href="https://github.com/jschr/textillate" target="_blank">https://github.com/jschr/textillate</a> (commit: e34a9938c5)';}
+}
