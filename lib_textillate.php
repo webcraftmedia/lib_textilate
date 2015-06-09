327bd4bfbc61979b98b01f7a1eaa9910dea01f77
Messerbill <ole.hoppe@gmx.de>
2015-06-09 22:06:24 +0200

    fixed: path to js file

--- lib_textillate.php
+++ lib_textillate.php
@@ -4,7 +4,7 @@ class lib_textillate extends \LIB\lib_js{
     public static function get_class(){
         return self::class;}
     public static function js_path(){
-        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'tablesorter/lib/jquery.textillate.js');}
+        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'textillate/lib/jquery.textillate.js');}
     public static function version(){
         return '<a href="https://github.com/jschr/textillate" target="_blank">https://github.com/jschr/textillate</a> (commit: e34a9938c5)';}
 }
